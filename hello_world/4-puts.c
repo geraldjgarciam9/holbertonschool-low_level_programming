5ff484c18f4a09474ac594b8e6ad726e51328012
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-20 15:51:26 -0700

    p

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *int main - prints "programming is like building a multilingual puzsle"
+ *main - prints "programming is like building a multilingual puzsle"
  *
- *Return 0 - devuelve el valor 0 como predeterminado
+ *Return: devuelve el valor 0 como predeterminado
  */
 int main(void)
 {
