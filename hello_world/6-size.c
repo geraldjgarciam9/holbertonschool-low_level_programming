4aad0899d74cadfc5ba88b275604f59faec9d9f5
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-20 17:52:55 -0700

    p

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - funcion que imprime los valores de los tipos de datos
+ *
+ *Return: devuelve 0
+ */
+int main (void)
+{
+printf(sizeof(char));
+printf(sizeof(int));
+printf(sizeof(float));
+printf(sizeof(double));
+printf(sizeof(short));
+printf(sizeof(long));
+printf(sizeof(long long));
+return (0);
+}
