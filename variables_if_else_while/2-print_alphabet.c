55cd8953b325100009068522f06747468f8024f1
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-22 12:09:11 -0700

    o

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - funcion principal
- * 
+ *
  * Return: retorna 0
  */
 int main(void)
 {
 char letra;
-for (letra = 'a'; letra <= 'z'; letra++) 
+for (letra = 'a'; letra <= 'z'; letra++)
 {
 putchar(letra);
 }
