bd0f62706bf563b7717c784278ef4222173beffd
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-22 13:35:31 -0700

    p

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - escribe los numeros del 0 al 9
+ * 
+ * Return: retorna 0
+ */
+int main(void)
+{
+int numero;
+for (numero = 0; numero <= 9; numero++)
+{
+printf("%d", numero);
+}
+printf("\n");
+return (0);
+}
