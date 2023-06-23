555def9b4977433098f797ba7517e503760a5037
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-23 08:02:26 -0700

    p

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/**
+ *imprimirAlfabeto - print alphabet
+ *
+ *Return: letra
+ **/
+void print_alphabet(void);
+/**
+ * main - funcion principal
+ *
+ *Return: retorna 0
+ **/
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+void print_alphabet(void)
+{
+char letra;
+for (letra = 'a'; letra <= 'z'; letra++) {
+putchar(letra);
+}
+putchar('\n');
+}#include <stdio.h>
+/**
+ *imprimirAlfabeto - print alphabet
+ *
+ *Return: letra
+ **/
+void print_alphabet(void);
+/**
+ * main - funcion principal
+ *
+ *Return: retorna 0
+ **/
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+void print_alphabet(void)
+{
+char letra;
+for (letra = 'a'; letra <= 'z'; letra++) {
+putchar(letra);
+}
+putchar('\n');
+}
