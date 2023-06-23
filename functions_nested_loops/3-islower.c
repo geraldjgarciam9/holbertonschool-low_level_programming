861fcc246827c6fb9221de59035049300a3d94c1
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-23 09:36:37 -0700

    p

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* _islower - imprime en total 10 veces el alfabetp
+*
+* Return: 1 si es minuscula y 0 si es otro caracter
+**/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
