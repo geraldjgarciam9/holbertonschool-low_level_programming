ac9a39aed3b5763030f435e8d60d750037db93fd
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-24 04:22:13 -0700

    p

---
+++ functions_nested_loops/5-singn.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/** 
+*print_sign - muestra el signo del  numero
+*@n: guarda el numero
+*
+* Return: 1 si es positivo,0 si es 0 y -1 si es negativo
+**/
+int print_sign(int n);
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+\n");
+return (1);
+}
+else if
+(n == 0) 
+{
+printf("0\n");
+return (0);
+}
+else
+{
+printf("-\n");
+return (-1);
+}
+}
