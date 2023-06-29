8795a2ebea28af8bc659f1d8607693285ec24baf
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-28 19:32:55 -0700

    p

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*print_to_98 - imprime todos los numeros hasta el 98 
+*@n: almacena el numero dado
+*
+*Return: devuelve el resultado en suma
+*/
+void print_to_98(int n);
+void print_to_98(int n)
+{
+while (n <= 98)
+{
+printf("%d, ", n);
+n++;
+} 
+while(n >= 98)
+{
+printf("%d, ", n);
+n--;
+}  
+}
