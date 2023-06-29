d52667ec4eda9ffcb6b9d1240e534c9366939be2
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-29 14:10:03 -0700

    s

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_puts - imprime una cadena
+*@str: almacena la cadena
+*
+*Return: el tamaño de la cadena
+*/
+void _puts(char *str);
+void _puts(char *str)
+{
+puts(str);
+}
