592428258161fdd0d9e476a2dc82aff5cc4d213d
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-28 18:52:43 -0700

    k

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*add - adiciona dos numeros 
+* 
+*Return: devuelve el resultado en suma
+*/
+int add(int, int);
+int add(int, int)
+{
+int a, b
+int suma = a + b;
+return (suma);
+}
