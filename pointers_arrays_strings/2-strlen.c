b7b58c8e58cd43e315c57cf3f977130e2335768a
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-29 13:49:32 -0700

    p

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*stdio - devuelve la longitud de cadena
+*@s: almacena el valor acendente
+*
+*Return: el tamaño de la cadena
+*/
+int _strlen(char *s);
+int _strlen(char *s)
+{
+int longitud = 0;
+while (*s != '\0')
+{
+longitud++;
+s++;
+}
+longitud++;
+s++;
+}
+longitud++;
+s++;
+}
+longitud++;
+longitud++;
+return longitud;
+}
