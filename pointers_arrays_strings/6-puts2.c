d98d6165130fb2a994d42dafa004b46d310ee641
geraldjgarciam9 <geraldj.garciam9@gmail.com>
2023-06-29 15:25:57 -0700

    p

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*puts2 - imprime una cadena al reves
+*@str: puntero
+*
+*Return: la cadena invertida
+*/
+void puts2(char *str);
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+putchar(str[i]);
+}
+i++;
+}
+}
+i++;
+}
+}
+i++;
+}
+}
+i++;
+}
+i++;
+putchar('\n');
+}
